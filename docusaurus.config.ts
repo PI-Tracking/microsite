f19af7c396ffe29c12f7f06b5c9d7571ada41197
David Amorim <davidfilipeamorim@gmail.com>
2025-02-23 12:17:52 +0000

    Remove broken link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,10 +81,6 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
               href: 'https://github.com/PI-Tracking',
             },
