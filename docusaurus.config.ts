8d70743494394c97e3af7cece18030076e788a6d
David Amorim <davidfilipeamorim@gmail.com>
2025-02-23 12:01:26 +0000

    Setup deployment to github pages
    Closes #2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://pi-tracking.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/microsite/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'PI-Tracking', // Usually your GitHub org/user name.
-  projectName: 'tracking-docs', // Usually your repo name.
+  projectName: 'microsite', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
